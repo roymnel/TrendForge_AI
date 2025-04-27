6a4040430fcc02ce9afe76e01fa3fb844491b45d
roymnel <thehopefullaugh@gmail.com>
2025-04-27 16:09:05 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,47 +2,50 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Home Page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# About Page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# Services Page
 @app.route('/services')
 def services():
     return render_template('service.html')
 
-# Projects Page
 @app.route('/projects')
 def projects():
     return render_template('project.html')
 
-# Blog Page
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
 
-# Team Page
 @app.route('/team')
 def team():
     return render_template('team.html')
 
-# Testimonial Page
 @app.route('/testimonial')
 def testimonial():
     return render_template('testimonial.html')
 
-# Contact Page
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-# 404 Page
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/thank-you')
+def thank_you():
+    return render_template('thank-you.html')
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
